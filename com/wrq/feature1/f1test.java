e3fd74022cb1a5424ac21f16bb33f0ee2cc58f7b
lxy225 <1182732534@qq.com>
2023-04-14 17:14:51 +0800

    对f1的任务第一次提交

---
+++ com/wrq/feature1/f1test.java
@@ -0,0 +1,14 @@
+package com.wrq.feature1;
+
+import java.util.HashMap;
+
+/**
+ * 分支feature1的任务
+ */
+public class f1test {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put(3,5);
+
+    }
+}
---
+++ com/wrq/feature1/f1test2.java
@@ -0,0 +1,10 @@
+package com.wrq.feature1;
+
+/**
+ * feature1的任务2
+ */
+public class f1test2 {
+    public static void main(String[] args) {
+
+    }
+}
